bf2ef00b00ccd358b8e62df8d40cfa7a7c8f86e0
Benji Marinacci <bcmarinacci@icloud.com>
2016-06-05 23:14:44 -0700

    Use arrow functions

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 'use strict';
 
-const trim = module.exports = function (str) {
-  return trim.trailing(trim.leading(str));
-};
-
-trim.leading = function (str) {
-  return str.replace(/^[ \t]+/gm, '');
-};
-
-trim.trailing = function (str) {
-  return str.replace(/[ \t]+$/gm, '');
-};
+const trim = module.exports = str => trim.trailing(trim.leading(str));
+trim.leading = str => str.replace(/^[ \t]+/gm, '');
+trim.trailing = str => str.replace(/[ \t]+$/gm, '');
