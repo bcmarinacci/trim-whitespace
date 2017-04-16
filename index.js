cb421cf4835353dd23b9a98edfa930e7814270e9
Benjamin Marinacci <bcmarinacci@icloud.com>
2017-04-15 23:42:46 -0700

    Use Prettier for formatting

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 
-const trim = module.exports = str => trim.trailing(trim.leading(str));
+const trim = str => trim.trailing(trim.leading(str));
+
 trim.leading = str => str.replace(/^[ \t]+/gm, '');
 trim.trailing = str => str.replace(/[ \t]+$/gm, '');
+
+module.exports = trim;
